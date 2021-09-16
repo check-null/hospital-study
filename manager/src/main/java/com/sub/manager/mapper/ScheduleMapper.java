f6c0b2a75540086c36bcca99e555a1801da3a0ce
checkNull <qw1124sdo8@live.com>
2021-09-16 18:14:39 +0800

    医院管理直接cv代码

---
+++ manager/src/main/java/com/sub/manager/mapper/ScheduleMapper.java
@@ -0,0 +1,10 @@
+package com.sub.manager.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sub.manager.model.Schedule;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScheduleMapper extends BaseMapper<Schedule> {
+
+}
