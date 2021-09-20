1e60d4cf7950696da777ade976f8f84cac644e43
Europa <qw1124sdo@vip.qq.com>
2021-09-20 22:43:20 +0800

    准备使用nacos

--- service/service_hospital/src/main/java/com/sub/hosp/ServiceHospApplication.java
+++ service/service_hospital/src/main/java/com/sub/hosp/ServiceHospApplication.java
@@ -3,11 +3,13 @@ package com.sub.hosp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.sub.hosp.mapper")
 @ComponentScan(basePackages = "com.sub")
+@EnableDiscoveryClient
 public class ServiceHospApplication {
 
     public static void main(String[] args) {
