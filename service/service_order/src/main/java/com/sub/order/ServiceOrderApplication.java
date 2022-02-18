b3723351ebd6c1ec096f8c844ca1df5d8ff235fb
checkNull <qw1124sdo8@live.com>
2022-02-18 18:02:12 +0800

    service-order模块少添加了依赖

--- service/service_order/src/main/java/com/sub/order/ServiceOrderApplication.java
+++ service/service_order/src/main/java/com/sub/order/ServiceOrderApplication.java
@@ -3,7 +3,11 @@ package com.sub.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = {"com.sub"})
+@EnableDiscoveryClient
 @MapperScan("com.sub.order.mapper")
 @SpringBootApplication
 public class ServiceOrderApplication {
